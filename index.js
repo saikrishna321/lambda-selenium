45ffe58c264c948d085d7e7354cd4bd789da99bd
ttllonsaikrisv <tllonsaikrisv@thetrainline.com>
2018-04-25 22:53:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+var Mocha = require('mocha'),
+    fs = require('fs'),
+    path = require('path');
+
+exports.handler = (event,context,callback) => {
+    let testUrl; 
+    testUrl = getTestUrl(event, testUrl);
+    var mocha = new Mocha();
+    
+    process.env['baseUrl'] = testUrl;
+    runMochaTests(mocha);
+    callback(null, {
+        statusCode: 200,
+        headers: { "x-custom-header" : "my custom header value" },
+        body: "hello world"
+    });
+};
+function runMochaTests(mocha) {
+    var testDir = './test/';
+    fs.readdirSync(testDir).filter(function (file) {
+        return file.substr(-3) === '.js';
+    }).forEach(function (file) {
+        mocha.addFile(path.join(testDir, file));
+    });
+    mocha.run(function (failures) {
+        process.on('exit', function () {
+            process.exit(failures);
+        });
+    });
+}
+
+function getTestUrl(event, testUrl) {
+    if (event.queryStringParameters.url === null) {
+        testUrl = event.url;
+    }
+    else {
+        testUrl = event.queryStringParameters.url;
+    }
+    return testUrl;
+}
+
---
+++ test/sampleWebdriver.js
@@ -0,0 +1,43 @@
+var assert = require("chai").assert;
+var webdriver = require("selenium-webdriver");
+var chrome = require("selenium-webdriver/chrome");
+const CHROMEDRIVER_PATH = "/var/task/lib/chromedriver";
+
+describe("Sample Webdriver test", function() {
+  it("Get Page title test", function() {
+    example(process.env.baseUrl);
+    assert.typeOf("sai", "string");
+  });
+
+  it("Failing Test", function() {
+    assert.typeOf(a, "string");
+  });
+});
+
+async function example(event) {
+  var builder = new webdriver.Builder().forBrowser("chrome");
+  var chromeOptions = new chrome.Options();
+  const defaultChromeFlags = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1280x1696", // Letter size
+    "--no-sandbox",
+    "--user-data-dir=/tmp/user-data",
+    "--hide-scrollbars",
+    "--enable-logging",
+    "--log-level=0",
+    "--v=99",
+    "--single-process",
+    "--data-path=/tmp/data-path",
+    "--ignore-certificate-errors",
+    "--homedir=/tmp",
+    "--disk-cache-dir=/tmp/cache-dir"
+  ];
+  chromeOptions.addArguments(defaultChromeFlags);
+  builder.setChromeOptions(chromeOptions);
+  var service = new chrome.ServiceBuilder(CHROMEDRIVER_PATH).build();
+  var driver = chrome.Driver.createSession(chromeOptions, service);
+    await driver.get(event);
+    let url = await driver.getTitle();
+    return await url;
+}
